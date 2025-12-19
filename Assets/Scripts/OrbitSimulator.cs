6d36dc7d37f57f3e59c891cc55ed272839d4c340
dahmanp <dahmanp@my.erau.edu>
2025-12-19 12:39:29 -0700

    Orbit Room First Implementation
    - customizable orbit speed
    - customizable scale
    - customizable date
    
    - start of the room for the orbit room (i created a new scene)

--- Assets/Scripts/OrbitSimulator.cs
+++ Assets/Scripts/OrbitSimulator.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿/*using System.Collections.Generic;
 using UnityEngine;
 
 public class OrbitSimulator : MonoBehaviour
@@ -108,4 +108,172 @@ public class OrbitSimulator : MonoBehaviour
                 p.body.transform.position = p.positions[index];
         }
     }
+}*/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitSimulator : MonoBehaviour
+{
+    [System.Serializable]
+    public class Planet
+    {
+        public string name;
+        public GameObject body;
+
+        [Header("Orbital Elements (J2000)")]
+        public double a;   // semi-major axis (km)
+        public double e;   // eccentricity
+        public double i;   // inclination (rad)
+        public double w;   // argument of periapsis (rad)
+        public double W;   // longitude of ascending node (rad)
+        public double Mo;  // mean anomaly at epoch (rad)
+    }
+
+    [Header("Planets")]
+    public Planet[] planets;
+    public Planet[] scaledPlanets;
+    public GameObject planetsParent;
+    public GameObject scaledPlanetsParent;
+    public bool scaled;
+
+    [Header("Simulation Start Date")]
+    public int startYear = 2025;
+    public int startMonth = 9;
+    public int startDay = 3;
+
+    [Header("Playback")]
+    public double timeScale = 86400.0; // 1 day per sec by default, this can be customizable
+    public bool playSimulation = true;
+
+    [Header("Unity Scaling")]
+    public float distanceScale = 1f / 1e8f;
+
+    const double mu = 1.32712440018e11; // Sun GM (km^3 / s^2)
+
+    double simulationTimeSeconds;
+    double epochJD;
+
+    // UNITY
+
+    void Start()
+    {
+        epochJD = JulianDate(2000, 1, 1);
+        ResetSimulation();
+    }
+
+    void Update()
+    {
+        if (playSimulation)
+            simulationTimeSeconds += Time.deltaTime * timeScale;
+
+        /*foreach (var planet in planets)
+        {
+            if (planet.body != null)
+            {
+                planet.body.transform.position =
+                    ComputeOrbitPosition(planet, simulationTimeSeconds);
+            }
+        }*/
+
+        if (scaled)
+        {
+            scaledPlanetsParent.SetActive(true);
+            planetsParent.SetActive(false);
+
+            foreach (var planet in scaledPlanets)
+            {
+                if (planet.body != null)
+                {
+                    planet.body.transform.position =
+                        ComputeOrbitPosition(planet, simulationTimeSeconds);
+                }
+            }
+        }
+        else
+        {
+            scaledPlanetsParent.SetActive(false);
+            planetsParent.SetActive(true);
+
+            foreach (var planet in planets)
+            {
+                if (planet.body != null)
+                {
+                    planet.body.transform.position =
+                        ComputeOrbitPosition(planet, simulationTimeSeconds);
+                }
+            }
+        }
+    }
+
+    // ORBIT CALCULATION
+
+    Vector3 ComputeOrbitPosition(Planet p, double t)
+    {
+        double n = System.Math.Sqrt(mu / (p.a * p.a * p.a));
+        double M = p.Mo + n * t;
+
+        double E = SolveKepler(p.e, M);
+
+        double theta = 2.0 * System.Math.Atan(
+            System.Math.Sqrt((1 + p.e) / (1 - p.e)) *
+            System.Math.Tan(E / 2.0)
+        );
+
+        double r = p.a * (1 - p.e * p.e) /
+                   (1 + p.e * System.Math.Cos(theta));
+
+        double x =
+            r * (System.Math.Cos(p.W) * System.Math.Cos(p.w + theta)
+            - System.Math.Sin(p.W) * System.Math.Sin(p.w + theta) * System.Math.Cos(p.i));
+
+        double y =
+            r * (System.Math.Sin(p.W) * System.Math.Cos(p.w + theta)
+            + System.Math.Cos(p.W) * System.Math.Sin(p.w + theta) * System.Math.Cos(p.i));
+
+        double z =
+            r * (System.Math.Sin(p.w + theta) * System.Math.Sin(p.i));
+
+        return new Vector3(
+            (float)x * distanceScale,
+            (float)z * distanceScale,
+            (float)y * distanceScale
+        );
+    }
+
+    double SolveKepler(double e, double M)
+    {
+        M %= 2.0 * System.Math.PI;
+        if (M < 0) M += 2.0 * System.Math.PI;
+
+        double E = M;
+
+        for (int i = 0; i < 30; i++)
+        {
+            double f = E - e * System.Math.Sin(E) - M;
+            double fp = 1 - e * System.Math.Cos(E);
+            E -= f / fp;
+        }
+
+        return E;
+    }
+
+    // DATE UTIL
+
+    double JulianDate(int y, int m, int d)
+    {
+        return 367 * y
+             - (7 * (y + (m + 9) / 12)) / 4
+             + (275 * m) / 9
+             + d + 1721013.5;
+    }
+
+    // RESET
+
+    public void ResetSimulation()
+    {
+        double startJD = JulianDate(startYear, startMonth, startDay);
+        simulationTimeSeconds = (startJD - epochJD) * 86400.0;
+    }
 }
+
